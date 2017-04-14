8da2d12f571b73e99133295a7854acb033d40c15
Khayrat Glende <khayrat.glende@neofonie.de>
2017-04-14 23:34:59 +0200

    added Dequeue.

--- assignment_2/src/main/java/Deque.java
+++ assignment_2/src/main/java/Deque.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Dequeue. A double-ended queue or deque (pronounced "deck")
@@ -7,6 +8,38 @@ import java.util.Iterator;
  */
 
 public class Deque<Item> implements Iterable<Item> {
+   private class FirstToLastIterator implements Iterator<Item> {
+      Node current;
+
+      FirstToLastIterator(Node first) {
+         current = first;
+      }
+
+      @Override
+      public boolean hasNext() {
+         return current.next != null;
+      }
+
+      @Override
+      public Item next() {
+         if (current == null) throw new NoSuchElementException();
+
+         Item item = current.item;
+         current = current.next;
+         return item;
+      }
+   }
+
+   private class Node {
+      Item item;
+      Node prev;
+      Node next;
+   }
+
+   private int count;
+   private Node first;
+   private Node last;
+
    /**
     * construct an empty deque
     */
@@ -19,7 +52,7 @@ public class Deque<Item> implements Iterable<Item> {
     * @return true if empty, false otherwise
     */
    public boolean isEmpty()     {
-      return true;
+      return count == 0;
    }
 
    /**
@@ -27,39 +60,73 @@ public class Deque<Item> implements Iterable<Item> {
     * @return the number of items on the deque
     */
    public int size()    {
-      return 0;
+      return count;
    }
 
    /**
     * add the item to the front
     * @param item - the item to add
     */
-   public void addFirst(Item item)          {
-
+   public void addFirst(Item item) {
+      if (item == null) throw new NullPointerException();
+      Node oldFirst = first;
+      first = new Node();
+      first.item = item;
+      first.next = oldFirst;
+      if (oldFirst != null) oldFirst.prev = first;
+      if (last == null) last = first;
+      count++;
    }
 
    /**
     * add the item to the end
     * @param item the item to add
     */
-   public void addLast(Item item)       {
-
+   public void addLast(Item item) {
+      if (item == null) throw new NullPointerException();
+      Node oldLast = last;
+      last = new Node();
+      last.item = item;
+      if (oldLast != null) oldLast.next = last;
+      if (first == null) first = last;
+      last.prev = oldLast;
+      count++;
    }
 
    /**
     * remove and return the item from the front
     * @return the removed item
     */
-   public Item removeFirst()        {
-      return null;
+   public Item removeFirst() {
+      if (count == 0) throw new NoSuchElementException();
+      Node oldFirst = first;
+      Item item = oldFirst.item;
+      first = oldFirst.next;
+      if (first != null) first.prev = null;
+      count--;
+      if (count == 0) {
+         last.prev = null;
+         last = null;
+      }
+      return item;
    }
 
    /**
     * remove and return the item from the end
     * @return the removed item.
     */
-   public Item removeLast()    {
-      return null;
+   public Item removeLast() {
+      if (count == 0) throw new NoSuchElementException();
+      Node oldLast = last;
+      Item item = oldLast.item;
+      last = oldLast.prev;
+      if (last != null) last.next = null;
+      count--;
+      if (count == 0) {
+         first.next = null;
+         first = null;
+      }
+      return item;
    }
 
    /**
@@ -67,7 +134,7 @@ public class Deque<Item> implements Iterable<Item> {
     * @return the iterator
     */
    public Iterator<Item> iterator()     {
-      return null;
+      return new FirstToLastIterator(first);
    }
 
    /**
--- assignment_2/src/test/java/DequeueTest.java
+++ assignment_2/src/test/java/DequeueTest.java
@@ -13,7 +13,7 @@ public class DequeueTest {
         Assert.assertTrue(dq.isEmpty());
     }
 
-    //@Test
+    @Test
     public void isEmptyAddFirstTest() {
         Deque<Integer> dq = new Deque<>();
 
@@ -23,12 +23,65 @@ public class DequeueTest {
     }
 
 
-    //@Test
+    @Test
     public void isEmptyAddFLastTest() {
         Deque<Integer> dq = new Deque<>();
 
         dq.addLast(1);
 
         Assert.assertFalse(dq.isEmpty());
+    }
+
+    @Test
+    public void addFirstRemoveFirstTest() {
+        Deque<String> dq = new Deque<>();
+        dq.addFirst("a");
+        Assert.assertEquals(dq.removeFirst(), "a");
+
+        Assert.assertTrue(dq.isEmpty());
+        Assert.assertEquals(dq.size(), 0);
+    }
+
+    @Test
+    public void addLastRemoveLastTest() {
+        Deque<String> dq = new Deque<>();
+        dq.addLast("a");
+        Assert.assertEquals(dq.removeLast(), "a");
+
+        Assert.assertTrue(dq.isEmpty());
+        Assert.assertEquals(dq.size(), 0);
+    }
+
+    @Test
+    public void addFirst2timesRemoveLastTest() {
+        Deque<String> dq = new Deque<>();
+        dq.addFirst("a");
+        dq.addFirst("b");
+
+        Assert.assertFalse(dq.isEmpty());
+        Assert.assertEquals(dq.size(), 2);
+
+        Assert.assertEquals(dq.removeLast(), "a");
+        Assert.assertFalse(dq.isEmpty());
+        Assert.assertEquals(dq.size(), 1);
+    }
+
+    @Test
+    public void iteratorTest() {
+        Deque<String> dq = new Deque<>();
+        String[] strings = {"a", "b", "c", "d", "e", "f", "g"};
+        for (String s : strings) {
+            dq.addLast(s);
+        }
+
+        int i = 0;
+        for (String s: dq) {
+            Assert.assertEquals(s, strings[i++]);
+        }
+
+        i = 0;
+        for (String s: dq) {
+            Assert.assertEquals(s, strings[i++]);
+        }
     }
 }
